202db8435d8be65a122a6c43acc207367daf326a
Johnson Earls <darkfoxprime@users.noreply.github.com>
2014-11-15 09:44:23 -0800

    Update darkfoxprime-akaplugin.c
    Add minimal documentation.  Fix a logic bug in the preference string scanner (next pointing to invalid data at end of string)

--- darkfoxprime-akaplugin.c
+++ darkfoxprime-akaplugin.c
@@ -2,6 +2,8 @@
 
 #include <glib.h>
 
+/* purple plugin include files */
+
 #include "debug.h"
 #include "conversation.h"
 #include "signals.h"
@@ -10,14 +12,26 @@
 #include "prefs.h"
 #include "version.h"
 
+/* define my plugin parameters */
+
 #define PLUGIN_ID "core-darkfoxprime-akaplugin"
 #define PREFS_ROOT "/plugins/core/" PLUGIN_ID
 #define PREFS_ALIASES PREFS_ROOT "/aliases"
 
+/* the default aliases preference value */
+
 #define DEFAULT_ALIASES "my name; nickname; other name; phase to alert me; etc..."
 
+/* My list of aliases.
+ * Maintained by the aliases_cb preference callback function
+ */
 static GSList *aliases_list = NULL ;
 
+/*  Process a chat message that is being received.
+ *  Check the contents of *buffer to see if it contains any of my aliases.
+ *  If it does, make *flags include the PURPLE_MESSAGE_NICK flag.
+ *  When done, return FALSE to indicate the message should *not* be ignored.
+ */
 static gboolean
 receiving_chat_msg_cb(PurpleAccount *account, char **sender, char **buffer, PurpleConversation *chat, PurpleMessageFlags *flags, void *data) {
     GSList *alias = aliases_list ;
@@ -32,10 +46,13 @@ receiving_chat_msg_cb(PurpleAccount *account, char **sender, char **buffer, Purp
     return FALSE ; /* don't ignore the message */
 }
 
-static guint aliases_cb_id = 0 ;
+/*  Process a change in the aliases preference.
+ *  Each time this preference changes, rebuild the aliases_list.
+ */
 static void
 aliases_cb(const char *name, PurplePrefType type, gconstpointer val, gpointer data) {
     gchar *start, *next, *end ;
+    /* destroy the current aliases_list, if any */
     if (aliases_list != NULL) {
         GSList *alias = aliases_list ;
         while (alias != NULL) {
@@ -44,7 +61,12 @@ aliases_cb(const char *name, PurplePrefType type, gconstpointer val, gpointer da
         }
         g_slist_free(aliases_list) ;
     }
+    /* start a new aliases_list */
     aliases_list = NULL ;
+    /* scan through the string.  start is the start of the alias name;
+     * end is one past the end of the name, and next either points to the
+     * NUL character or to one past the ';' at the end of the alias name.
+     */
     start = (gchar*) val ;
     while (*start) {
         if (*start == ' ') {
@@ -54,11 +76,13 @@ aliases_cb(const char *name, PurplePrefType type, gconstpointer val, gpointer da
             while ((*end) && (*end != ';')) {
                 end += 1 ;
             }
-            next = end + 1 ;
+            next = end;
+            if (*next == ';') { next += 1; }
             while ((end>start) && (*(end-1) == ' ')) {
                 end -= 1 ;
             }
             if (end > start) {
+                /* we have an alias.  Duplicate start..(end-1) in lower case (via g_ascii_strdown), and add it to the beginning of the aliases_list. */
                 aliases_list = g_slist_prepend( aliases_list, g_ascii_strdown( start, (gssize)(end - start) ) ) ;
                 purple_debug_info(PLUGIN_ID, "AKA Plugin found alias \"%s\".\n", (char*)(aliases_list->data)) ;
             }
@@ -67,35 +91,53 @@ aliases_cb(const char *name, PurplePrefType type, gconstpointer val, gpointer da
     }
 }
 
+/*  Initialize the plugin preferences.
+ *  Create the root preference directory if needed.
+ *  If the aliases preference exists, load it;
+ *  otherwise, initialize it from the default aliases value.
+ *  Then call the aliases_cb function to build the aliases_list.
+ */
 static void
 init_prefs(PurplePlugin *plugin) {
+    char *aliases;
     if (!purple_prefs_exists(PREFS_ROOT)) {
         purple_prefs_add_none(PREFS_ROOT) ;
     }
     if (purple_prefs_exists(PREFS_ALIASES)) {
-        aliases_cb(PREFS_ALIASES, PURPLE_PREF_STRING, purple_prefs_get_string(PREFS_ALIASES), NULL) ;
+        aliases = purple_prefs_get_string(PREFS_ALIASES) ;
     } else {
         purple_prefs_add_string(PREFS_ALIASES, DEFAULT_ALIASES) ;
+        aliases = DEFAULT_ALIASES ;
     }
-    aliases_cb_id = purple_prefs_connect_callback(plugin, PREFS_ALIASES, (PurplePrefCallback)(&aliases_cb), NULL) ;
-    aliases_cb(PREFS_ALIASES, PURPLE_PREF_STRING, DEFAULT_ALIASES, NULL) ;
+    aliases_cb(PREFS_ALIASES, PURPLE_PREF_STRING, aliases, NULL) ;
+    /* connect aliases_cb to be called whenever the preference changes */
+    purple_prefs_connect_callback(plugin, PREFS_ALIASES, (PurplePrefCallback)(&aliases_cb), NULL) ;
 }
 
+/*  Initialize the plugin.
+ */
 static void
 init_plugin(PurplePlugin *plugin) {
     init_prefs(plugin) ;
 }
 
+/*  Register any custom commands provided by the plugin.
+ */
 static void
 register_cmds(PurplePlugin *plugin) {
 }
 
+/*  Connect any signal handlers used by the plugin.
+ */
 static void
 connect_signals(PurplePlugin *plugin) {
     void *conv_handle = purple_conversations_get_handle() ;
     purple_signal_connect(conv_handle, "receiving-chat-msg", plugin, PURPLE_CALLBACK(receiving_chat_msg_cb), NULL) ;
 }
 
+/*  Load the plugin.
+ *  Register plugin commands and connect signal handlers.
+ */
 static gboolean
 plugin_load(PurplePlugin *plugin) {
     purple_debug_info(PLUGIN_ID, "AKA Plugin Loaded.\n") ;
@@ -104,12 +146,19 @@ plugin_load(PurplePlugin *plugin) {
     return TRUE ;
 }
 
+/*  Unload the plugin.
+ *  Currently does nothing.
+ */
 static gboolean
 plugin_unload(PurplePlugin *plugin) {
     purple_debug_info(PLUGIN_ID, "AKA Plugin Unloaded.\n") ;
     return TRUE ;
 }
 
+/*  Generate the preference frame for the plugin.
+ *  This consists of a header label, the aliases string input,
+ *  and an information label.
+ */
 static PurplePluginPrefFrame *
 prefs_info_cb(PurplePlugin *plugin) {
     PurplePluginPrefFrame *frame ;
@@ -130,6 +179,10 @@ prefs_info_cb(PurplePlugin *plugin) {
     return frame ;
 }
 
+/*  The preference information block.
+ *  The important part of this is the callback, which points to the
+ *  prefs_info_cb function to generate the preference frame.
+ */
 static PurplePluginUiInfo prefs_info = {
     /* callback */              prefs_info_cb,
     /* page_num (Reserved) */   0,
@@ -140,6 +193,7 @@ static PurplePluginUiInfo prefs_info = {
     /* Padding */               NULL
 } ;
 
+/*  The plugin information block.  */
 static PurplePluginInfo info = {
     /* magic */                 PURPLE_PLUGIN_MAGIC,
     /* compat major version */  PURPLE_MAJOR_VERSION,
@@ -173,4 +227,16 @@ static PurplePluginInfo info = {
     /* reserved */              NULL
 } ;
 
-PURPLE_INIT_PLUGIN(hello_world, init_plugin, info)
+/*  Initialize the plugin.
+ *
+ *  The first parameter to PURPLE_INIT_PLUGIN (akaplugin) is the plugin
+ *  name.  This is not a C string, nor is it a declared type or variable.
+ *  With PURPLE_PLUGINS defined, this argument is ignored.
+ *  If PURPLE_PLUGINS is not defined, this argument is used by the
+ *  PURPLE_INIT_PLUGIN as part of some internally-defined function names
+ *  (e.g. purple_init_akaplugin_plugin).
+ *
+ *  The second parameter is the plugin initialization function.
+ *  The third parameter is the plugin info block.
+ */
+PURPLE_INIT_PLUGIN(akaplugin, init_plugin, info)
