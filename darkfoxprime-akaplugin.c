8aaf85d4727129a7cc3ced44c114e9195bebb646
Johnson Earls <darkfoxprime@users.noreply.github.com>
2014-11-16 07:55:17 -0800

    Update darkfoxprime-akaplugin.c
    Cleaned up comments.  Corrected inconsistent naming for plugin_prefs_info block.
    
    First attempt at reworking aliases_changed_cb and receiving_chat_msg_cb to work with UTF-8 strings.

--- darkfoxprime-akaplugin.c
+++ darkfoxprime-akaplugin.c
@@ -1,8 +1,9 @@
 #define PURPLE_PLUGINS
 
 #include <glib.h>
+#include <string.h>
 
-/* purple plugin include files */
+/*  purple plugin include files  */
 
 #include "debug.h"
 #include "conversation.h"
@@ -12,7 +13,7 @@
 #include "prefs.h"
 #include "version.h"
 
-/* define my plugin parameters */
+/*  define my plugin parameters  */
 
 #define PLUGIN_ID "core-jme-akaplugin"
 #define PLUGIN_NAME "AKA Plugin"
@@ -25,12 +26,12 @@
 #define PREFS_ROOT "/plugins/core/" PLUGIN_ID
 #define PREFS_ALIASES PREFS_ROOT "/aliases"
 
-/* the default aliases preference value */
+/*  the default aliases preference value  */
 
 #define DEFAULT_ALIASES "my name; nickname; other name; phase to alert me; etc..."
 
-/* My list of aliases.
- * Maintained by the aliases_changed_cb preference callback function
+/*  My list of aliases.
+ *  Maintained by the aliases_changed_cb preference callback function.
  */
 static GSList *aliases_list = NULL ;
 
@@ -39,65 +40,135 @@ static GSList *aliases_list = NULL ;
  */
 static void
 aliases_changed_cb(const char *name, PurplePrefType type, gconstpointer val, gpointer data) {
-    /* character pointers to scan the preference value */
-    gchar *start, *next, *end ;
-    /* destroy the current aliases_list, if any */
+    /*  character pointers to scan the preference value.
+     *  "scan" will start at the beginning of the string and advance one
+     *  utf8 character at a time.
+     *  "start" will point to the first non-whitespace, non-separator
+     *  character found after the last separator (if any).
+     *  "end" will point to the last non-whitespace, non-separator
+     *  character found after "start".
+     */
+    gchar *scan, *start, *end ;
+    /*  the character we're scanning  */
+    gunichar this ;
+    /*  destroy the current aliases_list, if any  */
     if (aliases_list != NULL) {
-        /* the alias pointer to scan the list */
+        /*  the alias pointer to scan the list  */
         GSList *alias_ptr = aliases_list ;
         while (alias_ptr != NULL) {
-            /* free the string for this list entry */
+            /*  free the string for this list entry  */
             free(alias_ptr->data) ;
-            /* move to the next list entry */
+            /*  move to the next list entry  */
             alias_ptr = alias_ptr->next ;
         }
-        /* free up the list entries themselves */
+        /*  free up the list entries themselves  */
         g_slist_free(aliases_list) ;
     }
-    /* start a new aliases_list */
+    /*  start a new aliases_list  */
     aliases_list = NULL ;
-    /* scan through the string.  start is the start of the alias name;
-     * end is one past the end of the name, and next either points to the
-     * NUL character or to one past the ';' at the end of the alias name.
+    /*  scan through the string.
+     *
+     *  If scan finds a separator character and start is not NULL, add
+     *  the lowercased version of the substring start..end to
+     *  aliases_list.  Then reset start and end to NULL.
+     *  At the end of the scan, if start is not NULL, add the lowercased
+     *  version of the final substring to aliases_list.
      */
-    start = (gchar*) val ;
-    /* loop until we're looking at a NUL character */
-    while (*start != '\0') {
-        /* skip spaces */
-        if (*start == ' ') {
-            start += 1 ;
-        } else {
-            /*  we're at the start of an alias - or at a ';' with no
-             *  intervening alias.  In either case, scan until we find
-             *  our ';'.
-             */
-            end = start ;
-            while ((*end) && (*end != ';')) {
-                end += 1 ;
+    scan = (gchar*) val ;
+    start = end = (gchar*) NULL ;
+    /*  scan through the string  */
+    for (   scan = (gchar*) val ;
+            ((this = g_utf8_get_char(scan)) != 0) ;
+            scan = g_utf8_next_char(scan)
+     ) {
+        /*  skip spaces  */
+        if (!g_unichar_isspace(this)) {
+            /*  if we're at a separator, process the substring  */
+            if (this == ';') {
+                /*  If we have a non-empty alias, add it to aliases_list.  */
+                if (start != NULL) {
+                    /*  calculate the number of bytes between start
+                     *  and the character after end.
+                     */
+                    gssize sublen = g_utf8_next_char(end) - start;
+                    /*  Use g_utf8_strdown to both duplicate and
+                     *  transform to lowercase the contents of the
+                     *  substring starting at start and including sublen
+                     *  characters, and add the duplicated string to the
+                     *  beginning of the aliases_list.
+                     */
+                    aliases_list = g_slist_prepend( aliases_list, g_utf8_strdown( start, sublen ) ) ;
+                    purple_debug_info(PLUGIN_ID, "AKA Plugin found %d byte alias \"%s\".\n", (int)sublen, (gchar*)(aliases_list->data)) ;
+                    /*  reset start and sublen for next substring  */
+                    start = (gchar*)NULL ;
+                    sublen = 0 ;
+                }
+            } else {
+                /*  not a separator, update start and end appropriately  */
+                if (start == NULL) {
+                    start = scan ;
+                }
+                end = scan ;
             }
-            /*  skip past any ';' to mark the next alias to check.  */
-            next = end ;
-            if (*next == ';') { next += 1 ; }
-            /*  shift end backwards over any trailing whitespace.  */
-            while ((end>start) && (*(end-1) == ' ')) {
-                end -= 1 ;
-            }
-            /*  If we have a non-empty alias, add it to aliases_list.  */
-            if (end > start) {
-                /*  Use g_ascii_strdown to both duplicate and transform
-                 *  to lowercase the contents of the string between start
-                 *  and end, and add the duplicated string to the
-                 *  beginning of the aliases_list.
-                 */
-                aliases_list = g_slist_prepend( aliases_list, g_ascii_strdown( start, (gssize)(end - start) ) ) ;
-                purple_debug_info(PLUGIN_ID, "AKA Plugin found alias \"%s\".\n", (char*)(aliases_list->data)) ;
-            }
-            /*  continue with the next potential alias.  */
-            start = next ;
         }
+    }
+    /*  End of the string.
+     *  If we have a non-empty alias, add it to aliases_list.
+     */
+    if (start != NULL) {
+        /*  calculate the number of bytes between start and the character
+         *  after end.
+         */
+        gssize sublen = g_utf8_next_char(end) - start;
+        /*  Use g_utf8_strdown to both duplicate and transform to
+         *  lowercase the contents of the substring starting at start and
+         *  including sublen characters, and add the duplicated string to
+         *  the beginning of the aliases_list.
+         */
+        aliases_list = g_slist_prepend( aliases_list, g_utf8_strdown( start, sublen ) ) ;
+        purple_debug_info(PLUGIN_ID, "AKA Plugin found %d byte alias \"%s\".\n", (int)sublen, (gchar*)(aliases_list->data)) ;
     }
     /*  Done, nothing to return.  */
     return ;
+}
+
+/*  Determine where, if at all, a UTF8 substring occurs within another
+ *  UTF8 string.  Return a pointer to the start of the substring, or
+ *  NULL if not found.
+ */
+static gchar*
+find_utf8_substr(gchar *str, gchar *substr) {
+    /*  record the first character of substr for scanning  */
+    gunichar first = g_utf8_get_char(substr) ;
+    /*  record the collation key for substr for comparison  */
+    gchar* substr_key = g_utf8_collate_key(substr, -1) ;
+    /*  record the length of substr, in bytes, for comparison.  do this by finding the length, in utf8 characters, converting that to a pointer, then using pointer arithmetic to find the byte length.  */
+    gssize len = g_utf8_offset_to_pointer(substr, g_utf8_strlen(substr, -1)) - substr ;
+    /*  find the first occurrence of 'first'  */
+    str = g_utf8_strchr(str, -1, first) ;
+    /*  as long as we have not run out of string to check,
+     *  and we are not pointing at an occurrence of substr,
+     *  continue scanning
+     */
+    while (str != NULL) {
+        /*  compute the collation key for the 'len'-length
+         *  substring located at 'str'
+         */
+        gchar* this_key = g_utf8_collate_key(str, len) ;
+        /*  get our comparison value, then free the key  */
+        int  this_comp = strcmp(substr_key, this_key) ;
+        g_free(this_key) ;
+        /*  if comparison was 0 (equal), break out of the scan loop  */
+        if (this_comp == 0) {
+            break ; /* out of while (str != NULL) loop */
+        }
+        /*  move str to the next occurrence of 'first'  */
+        str = g_utf8_strchr(g_utf8_next_char(str), -1, first) ;
+    }
+    /*  free up the collation key  */
+    g_free(substr_key) ;
+    /*  return the result  */
+    return(str) ;
 }
 
 /*  Process a chat message that is being received.
@@ -107,26 +178,29 @@ aliases_changed_cb(const char *name, PurplePrefType type, gconstpointer val, gpo
  */
 static gboolean
 receiving_chat_msg_cb(PurpleAccount *account, char **sender, char **buffer, PurpleConversation *chat, PurpleMessageFlags *flags, void *data) {
-    /* pointer into the aliases list */
+    /*  pointer into the aliases list  */
     GSList *alias_ptr = aliases_list ;
-    /* a copy of the message transformed to lowercase */
-    char *lcmsg = g_ascii_strdown( *buffer, -1 ) ;
-    /* loop through each alias and check to see if it exists in the message */
+    /*  a copy of the [utf8] message transformed to lowercase  */
+    gchar *lcmsg = g_utf8_strdown( *buffer, -1 ) ;
+    purple_debug_info(PLUGIN_ID, "AKA Plugin checking message \"%s\".\n", lcmsg) ;
+    /*  loop through each alias and check to see if it exists in the message  */
     while (alias_ptr != NULL) {
-        if (g_strrstr( lcmsg, alias_ptr->data ) != NULL) {
+        purple_debug_info(PLUGIN_ID, "AKA Plugin checking against \"%s\".\n", (char*)alias_ptr->data) ;
+        if (find_utf8_substr(lcmsg, alias_ptr->data) != NULL) {
+            purple_debug_info(PLUGIN_ID, "AKA Plugin found a match!\n") ;
             /*  The alias exists; set the NICK flag to indicate this
              *  message was directed at us, and break out of the alias
              *  loop.
              */
             *flags |= PURPLE_MESSAGE_NICK ;
-            break ; /* out of while (alias_ptr != NULL) loop */
+            break ; /*  out of while (alias_ptr != NULL) loop  */
         }
-        /* remember to not infinite loop on the same alias pointer... */
+        /*  remember to not infinite loop on the same alias pointer...  */
         alias_ptr = alias_ptr->next ;
     }
-    /* free our lowercase copy of the message */
+    /*  free our lowercase copy of the message  */
     free(lcmsg) ;
-    /* return FALSE says the message should not be ignored. */
+    /*  return FALSE says the message should not be ignored.  */
     return FALSE ;
 }
 
@@ -135,8 +209,8 @@ receiving_chat_msg_cb(PurpleAccount *account, char **sender, char **buffer, Purp
  */
 static void
 register_cmds(PurplePlugin *plugin) {
-    /* no commands registered at this time */
-    /* Done, nothing to return */
+    /*  no commands registered at this time  */
+    /*  Done, nothing to return  */
     return ;
 }
 
@@ -145,11 +219,11 @@ register_cmds(PurplePlugin *plugin) {
  */
 static void
 connect_signals(PurplePlugin *plugin) {
-    /* The conversation handle is used for conversation-related signals */
+    /*  The conversation handle is used for conversation-related signals  */
     void *conv_handle = purple_conversations_get_handle() ;
-    /* Register the receiving-chat-msg signal handler */
+    /*  Register the receiving-chat-msg signal handler  */
     purple_signal_connect(conv_handle, "receiving-chat-msg", plugin, PURPLE_CALLBACK(receiving_chat_msg_cb), NULL) ;
-    /* Done, nothing to return */
+    /*  Done, nothing to return  */
     return ;
 }
 
@@ -162,22 +236,24 @@ connect_signals(PurplePlugin *plugin) {
 static void
 init_prefs(PurplePlugin *plugin) {
     const char *aliases ;
-    /* If the root preference directory does not exist, create it */
+    /*  If the root preference directory does not exist, create it  */
     if (!purple_prefs_exists(PREFS_ROOT)) {
         purple_prefs_add_none(PREFS_ROOT) ;
     }
-    /* If the alias preference exists, load it, otherwise create it from the default value */
+    /*  If the alias preference exists, load it, otherwise create it from the default value  */
     if (purple_prefs_exists(PREFS_ALIASES)) {
         aliases = purple_prefs_get_string(PREFS_ALIASES) ;
     } else {
         purple_prefs_add_string(PREFS_ALIASES, DEFAULT_ALIASES) ;
         aliases = DEFAULT_ALIASES ;
     }
-    /* initialize the alias list from the alias value loaded (or created) */
+    /*  initialize the alias list from the alias value loaded (or created)  */
     aliases_changed_cb(PREFS_ALIASES, PURPLE_PREF_STRING, aliases, NULL) ;
-    /* connect aliases_changed_cb to be called whenever the preference changes */
+    /*  connect aliases_changed_cb to be called whenever the preference
+     *  changes
+     */
     purple_prefs_connect_callback(plugin, PREFS_ALIASES, (PurplePrefCallback)(&aliases_changed_cb), NULL) ;
-    /* Done, nothing to return */
+    /*  Done, nothing to return  */
     return ;
 }
 
@@ -187,9 +263,9 @@ init_prefs(PurplePlugin *plugin) {
 static void
 init_plugin_hook(PurplePlugin *plugin) {
     purple_debug_info(PLUGIN_ID, "AKA Plugin Initialized.\n") ;
-    /* Initialize the plugin's preferences */
+    /*  Initialize the plugin's preferences  */
     init_prefs(plugin) ;
-    /* Done, nothing to return */
+    /*  Done, nothing to return  */
     return ;
 }
 
@@ -199,11 +275,11 @@ init_plugin_hook(PurplePlugin *plugin) {
 static gboolean
 plugin_load_hook(PurplePlugin *plugin) {
     purple_debug_info(PLUGIN_ID, "AKA Plugin Loaded.\n") ;
-    /* register any custom plugin commands */
+    /*  register any custom plugin commands  */
     register_cmds(plugin) ;
-    /* register any signal handlers */
+    /*  register any signal handlers  */
     connect_signals(plugin) ;
-    /* return TRUE says continue loading the plugin */
+    /*  return TRUE says continue loading the plugin  */
     return TRUE ;
 }
 
@@ -214,8 +290,8 @@ plugin_load_hook(PurplePlugin *plugin) {
 static gboolean
 plugin_unload_hook(PurplePlugin *plugin) {
     purple_debug_info(PLUGIN_ID, "AKA Plugin Unloaded.\n") ;
-    /* We don't need to do anything special here */
-    /* return TRUE says continue unloading the plugin */
+    /*  We don't need to do anything special here  */
+    /*  return TRUE says continue unloading the plugin  */
     return TRUE ;
 }
 
@@ -228,33 +304,33 @@ prefs_info_hook(PurplePlugin *plugin) {
     PurplePluginPrefFrame *pref_frame ;
     PurplePluginPref *pref_item ;
 
-    /* the frame in which the preferences will be presented */
+    /*  the frame in which the preferences will be presented  */
     pref_frame = purple_plugin_pref_frame_new() ;
 
-    /* the header label */
+    /*  the header label  */
     pref_item = purple_plugin_pref_new_with_label("Enter your aliases, separated by semicolons (;), in this text box.") ;
     purple_plugin_pref_frame_add(pref_frame, pref_item) ;
 
-    /* The input box for the list of aliases */
+    /*  The input box for the list of aliases  */
     pref_item = purple_plugin_pref_new_with_name_and_label(
         PREFS_ALIASES, "Aliases"
     ) ;
     purple_plugin_pref_set_max_length(pref_item, 256) ;
     purple_plugin_pref_frame_add(pref_frame, pref_item) ;
 
-    /* The information label describing the content of the aliases */
+    /*  The information label describing the content of the aliases  */
     pref_item = purple_plugin_pref_new_with_label("Any chat message that contains one of the words or phrases here\nwill be treated as if it was addressed to your username.") ;
     purple_plugin_pref_frame_add(pref_frame, pref_item) ;
 
-    /* return the finished frame */
+    /*  return the finished frame  */
     return pref_frame ;
 }
 
 /*  The preference information block.
  *  The important part of this is the callback, which points to the
- *  prefs_info_cb function to generate the preference frame.
+ *  prefs_info_hook function to generate the preference frame.
  */
-static PurplePluginUiInfo prefs_info = {
+static PurplePluginUiInfo plugin_prefs_info = {
     /* get_plugin_pref_frame */ &prefs_info_hook ,
     /* page_num (Reserved) */   0 ,
     /* frame (Reserved) */      NULL ,
