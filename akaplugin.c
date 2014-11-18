ca509b776e9eae6c2eff8b1ed87159e9ee505928
Johnson Earls <darkfoxprime@users.noreply.github.com>
2014-11-17 18:47:16 -0800

    Update and rename darkfoxprime-akaplugin.c to akaplugin.c
    Remove email address; rename to akaplugin.c

--- akaplugin.c
+++ akaplugin.c
@@ -18,7 +18,7 @@
 #define PLUGIN_ID "core-jme-akaplugin"
 #define PLUGIN_NAME "AKA Plugin"
 #define PLUGIN_VERSION "0.2"
-#define PLUGIN_AUTHOR "Johnson Earls <darkfoxprime@gmail.com>"
+#define PLUGIN_AUTHOR "Johnson Earls"
 #define PLUGIN_URL "https://github.com/darkfoxprime/akaplugin/wiki"
 #define PLUGIN_SUMMARY "Recognize aliases said in chats as also being you."
 #define PLUGIN_DESCRIPTION "This plugin listens for chat messages and, when a message contains any of the aliases configured in its preferences, marks the message as being said to you."
